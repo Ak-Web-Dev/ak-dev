253f37fc4f57cab4c29e9ec184de3fd0ab04a2d0
AK-Dev. <imakbss@gmail.com>
2024-06-30 17:04:36 +0500

    a

---
+++ script.js
@@ -0,0 +1,12 @@
+let popup = document.getElementById("popup");
+function pop(){
+    popup.classList.add("open-popup")
+} 
+function closepopup(){
+    popup.classList.remove("open-popup")
+}
+
+function mail(){
+    alert("Gmail Address: imakbss@gmail.com")
+    alert('Discord')
+}
